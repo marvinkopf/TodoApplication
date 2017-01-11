7326c81a0cecb96ba53e8bc3b7195b4fcd7bbf37
Marvin Kopf <marvinkopf@posteo.de>
2017-01-11 23:40:34 +0100

    move app behind homepage

---
+++ Controllers/AppController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Todoapp.Controllers
+{
+    [Route("app")]
+    public class AppController : Controller
+    {
+        [HttpGet]
+        public IActionResult App() => View();
+    }
+}
