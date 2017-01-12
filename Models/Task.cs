b7abba32bed2e178e60a1676c5bbc27bcd576d93
Marvin Kopf <marvinkopf@posteo.de>
2017-01-12 01:40:33 +0100

    add task entity

---
+++ Models/Task.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Todoapp.Models
+{
+    public class TaskItem
+    {
+        public int TaskItemId { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsDeleted { get; set; }
+        
+        public bool IsCompleted { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public DateTimeOffset CreatedTime { get; set; }
+
+        public DateTimeOffset CompletedTime { get; set; }
+
+        public DateTimeOffset DeletedTime { get; set; }
+    }
+}
