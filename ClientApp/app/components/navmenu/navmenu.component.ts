684ac4d51b4116d8fcac7e63083e7e7029dd7bb2
Marvin Kopf <marvinkopf@posteo.de>
2017-01-28 20:45:44 +0100

    add user menu shell

--- ClientApp/app/components/navmenu/navmenu.component.ts
+++ ClientApp/app/components/navmenu/navmenu.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from "./../../core/services/userService";
+import { User } from "./../../core/domain/user";
 
 @Component({
     selector: 'nav-menu',
     templateUrl: './navmenu.component.html',
-    styleUrls: ['./navmenu.component.css']
+    styleUrls: ['./navmenu.component.css'],
+    providers: [UserService]
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
+    errorMessage: String;
+    public userName: String;
+
+    constructor(private userService: UserService) { }
+
+    ngOnInit() {
+        this.getUserInfo();
+    }
+
+    logout() {
+        this.userService.logout().subscribe();
+    }
+
+    getUserInfo() {
+        this.userService.getUserInfo().subscribe(
+        user => this.userName = user.userName,
+        error => this.errorMessage = <any>error);
+    }
 }
